c9c763fedb1fa1ca5c9a77e7656f8e403d911923
Barth. Feudong <61944369+mrSchaffman@users.noreply.github.com>
2023-07-31 10:03:14 +0200

    setting the basic layout of the App..

---
+++ reactjs/timer_app/src/components/TimersDashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+export default class TimersDashboard extends React.Component{
+    render() {
+        return (
+            <div className='ui centered grid'>
+                <div className='column'>
+
+                </div>
+            </div>
+        );
+    }
+}
